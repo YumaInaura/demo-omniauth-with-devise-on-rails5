caee2e5bc70119b9a7f99cbd8272288c11f7b875
Yuma inaura <yuma.inaura@gmail.comm>
2017-05-07 14:43:17 +0900

    Fix twitter login overflow cookie
    Error:
    
    ActionDispatch::Cookies::CookieOverflow in Users::OmniauthCallbacksController#twitter
    
    See http://easyramble.com/cookie-overflow-on-rails.html

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,7 +18,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, :event => :authentication
       set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
     else
-      session["devise.twitter_data"] = request.env["omniauth.auth"]
+      session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to root_path
     end
   end
