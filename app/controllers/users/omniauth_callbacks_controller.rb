b50d177227381797a00abbf00feda835e49edc89
Yuma inaura <yuma.inaura@gmail.comm>
2017-05-07 14:31:07 +0900

    Add Users::OmniauthCallbacksController#twitter

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def slack
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_slack_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
@@ -9,6 +9,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     else
       session["devise.slack_data"] = request.env["omniauth.auth"]
       redirect_to root_path
+    end
+  end
+
+  def twitter
+  # You need to implement the method below in your model (e.g. app/models/user.rb)
+  @user = User.from_twitter_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
+    else
+      session["devise.twitter_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
          :omniauth_providers => [:slack, :twitter]
 
-  def self.from_omniauth(auth)
+  def self.from_slack_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
+      user.image = auth.info.image
+    end
+  end
+
+  def self.from_twitter_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.first_name = auth.info.name
       user.image = auth.info.image
     end
   end
