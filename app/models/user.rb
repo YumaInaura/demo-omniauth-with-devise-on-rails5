ef973c38ff8d85e270087f976a0c781201601a37
Yuma inaura <yuma.inaura@gmail.comm>
2017-05-07 15:16:37 +0900

    Change first_or_create to first_or_create!

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
          :omniauth_providers => [:slack, :twitter]
 
   def self.from_slack_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.first_name = auth.info.first_name
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   end
 
   def self.from_twitter_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.first_name = auth.info.name
